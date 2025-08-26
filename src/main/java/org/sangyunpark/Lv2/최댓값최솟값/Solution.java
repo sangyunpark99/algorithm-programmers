f7ae4eecc189ab839b7f6fd4a00ab69654117534
sangyunpark99 <psangyun5108@gmail.com>
2025-08-26 23:05:14 +0900

    Lv2.최댓값과 최솟값 정답 풀이

---
+++ src/main/java/org/sangyunpark/Lv2/최댓값최솟값/Solution.java
@@ -0,0 +1,17 @@
+package org.sangyunpark.Lv2.최댓값최솟값;
+
+import java.util.*;
+
+public class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        int[] array = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray(); // 문자열 int[] 배열로 변환
+
+        Arrays.sort(array); // 변환환 배열을 정렬
+        sb.append(array[0]).append(" "); // 최솟값과 띄워쓰기 문자를 StringBuilder에 추가
+        sb.append(array[array.length - 1]); // 최댓값을 StringBuilder에 추가
+
+        return sb.toString(); // 정답 출력
+    }
+}
