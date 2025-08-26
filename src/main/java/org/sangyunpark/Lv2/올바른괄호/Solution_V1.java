2129886aa3f74cdaf9e029d2d5e296d0cf04acbf
sangyunpark99 <psangyun5108@gmail.com>
2025-08-27 08:41:44 +0900

    LV2 올바른 괄호 문제풀이

---
+++ src/main/java/org/sangyunpark/Lv2/올바른괄호/Solution_V1.java
@@ -0,0 +1,30 @@
+package org.sangyunpark.Lv2.올바른괄호;
+
+import java.util.*;
+
+public class Solution_V1 {
+
+    private Stack<Character> stack = new Stack<>();
+
+    boolean solution(String s) {
+        boolean answer = true;
+
+        for(char value : s.toCharArray()) {
+            if(stack.isEmpty()) { // stack이 비어있는 경우
+                if(value == ')') { // 처음부터 닫히는 문자가 들어온 경우
+                    return false;
+                }
+                stack.push(value); // 값을 집어 넣기
+            }else { // 이미 stack에 값이 존재하는 경우
+                if(value == ')') { // 닫히는 문자열인 경우
+                    stack.pop();
+                } else { // 열리는 문자열인 경우
+                    stack.push(value);
+                }
+            }
+        }
+
+        if(stack.isEmpty()) return true; // 스택이 비어있는 경우? 짝이 전부 존재하는 경우
+        return false;
+    }
+}
---
+++ src/main/java/org/sangyunpark/Lv2/올바른괄호/Solution_V2.java
@@ -0,0 +1,14 @@
+package org.sangyunpark.Lv2.올바른괄호;
+
+public class Solution_V2 {
+    boolean solution(String s) {
+        int answer = 0;
+
+        for(char value : s.toCharArray()) {
+            answer = value == ')' ? answer-1 : answer + 1;
+            if(answer < 0) return false;
+        }
+
+        return answer == 0;
+    }
+}
