8be8b6b539c99ff4a72ef22e72cb5cf00fae02a9
psangyun5108@gmail.com <psangyun5108@gmail.com>
2025-09-10 14:17:23 +0900

    Lv3 가장 긴 팰린드롬 투 포인터 풀이 코드

---
+++ src/main/java/org/sangyunpark/Lv3/가장긴팰린드롬/Solution.java
@@ -0,0 +1,41 @@
+package org.sangyunpark.Lv3.가장긴팰린드롬;
+
+public class Solution {
+  private int answer = 1;
+
+  public int solution(String s)
+  {
+    char[] arr = s.toCharArray();
+
+    int n = s.length();
+    if(n == 1) return 1;
+
+    for(int i = 0; i < arr.length; i++) { // 기준
+
+      int acnt = 1;
+      int left = i - 1;
+      int right = i + 1;
+      while(left >= 0 && right < arr.length && arr[left] == arr[right]) {
+        acnt += 2;
+        left--;
+        right++;
+      }
+
+      answer = Math.max(answer, acnt);
+
+      int bcnt = 0;
+      left = i;
+      right = i + 1;
+      while(left >= 0 && right < arr.length && arr[left] == arr[right]) {
+        bcnt += 2;
+        left--;
+        right++;
+      }
+
+      answer = Math.max(answer, bcnt);
+      // 짝수개
+    }
+
+    return answer;
+  }
+}
